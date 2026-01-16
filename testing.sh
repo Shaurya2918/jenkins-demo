f974a0e2bcb1768f70855d9dfa9a45f4235efa1f
Shaurya2918 <shaurya.negi1991@gmail.com>
2026-01-16 11:23:46 +0530

    Update InM in testing.sh

--- testing.sh
+++ testing.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-V1=`(pgrep InMemo)`
+V1=`(pgrep InM)`
 V2=""
 if [ "$V1" == "$V2" ]
    then
-echo "InMemory is stopped, cannot create Gstack"
+echo "InM is stopped, cannot create Gstack"
    else
 gstack $V1 > ./gstack/gstack_$(date +%d-%h-%y_%H-%M-%S).txt
 fi
